dfb4ce9235b00403922d861593af546f46f6468d
Diogo Souza <caravanaflavio@gmail.com>
2026-08-12 19:56:38 -0300

    Configura Android e prepara Firebase

---
+++ android/app/src/main/java/br/com/caravanaflavio/bagagensbarretao/MainActivity.java
@@ -0,0 +1,5 @@
+package br.com.caravanaflavio.bagagensbarretao;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.caravanaflavio.bagagensbarretao',
+  appName: 'Bagagens Barret\u00e3o',
+  webDir: 'dist',
+};
+
+export default config;
